cd19bb534895dda36f27fc39e3f6f2692236605d
Mustafa Lanewala <125785421+mustafalanewala@users.noreply.github.com>
2024-11-19 08:49:12 +0530

    1.Variables

---
+++ 01_Variables.js
@@ -0,0 +1,12 @@
+const id = 3008
+let fullname = "mustafa"
+var age = 20 // WE DONT USE VAR IN JS BCZ OF SCOPE
+profession = "Engineer"
+let email
+
+//id = 2024 // CONST NOT ALLOWED TO CHANGE
+fullname = "mustafa lanewala"
+age = 22;
+console.log(id);
+console.log(fullname);
+console.table([id , fullname , age , profession , email]);
