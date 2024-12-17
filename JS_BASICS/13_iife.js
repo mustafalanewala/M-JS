91f7ea44fd07e729b0651a9c5b0ff1f4a68a981c
Mustafa Lanewala <125785421+mustafalanewala@users.noreply.github.com>
2024-12-17 19:41:30 +0530

    modified 13.iife

--- JS_BASICS/13_iife.js
+++ JS_BASICS/13_iife.js
@@ -1,16 +1,30 @@
 // Immediately Invoked Function Expression (IIFE)
 
-(function DB(){
-    // Named IIFE
-    console.log('DB CONNECTED');
-})(); // Semi-Colon is Imp to end 
+(function DB() {
+  // Named IIFE
+  console.log("DB CONNECTED");
+})(); // Semi-Colon is Imp to end
 
 (() => {
-    // Simple IIFE
-    console.log('DB CONNECTED...');
+  // Simple IIFE
+  console.log("DB CONNECTED...");
 })();
 
-( (name) => {
-    // Simple IIFE With Parameters
-    console.log(`${name} DB CONNECTED...`);
-} )('Mustafa')
+((name) => {
+  // Simple IIFE With Parameters
+  console.log(`${name} DB CONNECTED...`);
+})("Mustafa");
+
+// It is used to create private variables
+let ans = (function () {
+  let privateKey = 53;
+
+  return {
+    getter: function () {
+      console.log(privateKey);
+    },
+    setter: function (val) {
+      privateKey = val;
+    },
+  };
+})();
